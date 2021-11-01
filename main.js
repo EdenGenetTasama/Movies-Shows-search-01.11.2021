4ddc5b48b557be81ff522cdbd2abc3c2a84cfab4
EdenGenetTasama <edentasama19@gmail.com>
2021-11-01 18:56:33 +0200

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const basicAPI = "https://api.tvmaze.com/"; //! full info "/schedule/full";
 
-//! /:id
-
-// console.log(`${basicAPI}/schedule/full`);
+//! /bilding
 
 async function getMovieInfoAll() {
-  try {
+  try { gifLoading()
     return await fetch(
-      `${basicAPI}/search/${selectId.value}?q=${inputId.value}`
+     `${basicAPI}/search/${selectId.value}?q=${inputId.value}`
     ).then((response) => response.json());
   } catch (error) {
     return error;
@@ -16,7 +14,8 @@ async function getMovieInfoAll() {
 
 async function sortById(id) {
   try {
-    return await fetch(`${basicAPI}/search/${id}`).then((response) =>
+    gifLoading();
+    return await fetch(`${basicAPI}people/${id}`).then((response) =>
       response.json()
     );
   } catch (error) {
@@ -24,53 +23,84 @@ async function sortById(id) {
   }
 }
 
+async function sortByName(name) {
+  try {
+    gifLoading();
+    return await fetch(`${basicAPI}/search/people?q=${name}`).then((response) =>
+      response.json()
+    );
+  } catch (error) {
+    return error;
+  }
+}
+
+function gifLoading() {
+  DivOfShowInfo.innerHTML = `<img src="./images/loading1 gif.gif" id="gifLoadingImg">`;
+}
+
+function stopGifLoading() {
+  gifLoadingImg.style.display = "none";
+}
+
 function runAllArrayOfObject(arrayOfObject) {
   for (const item of arrayOfObject) {
-    console.log(item);
+    // console.log(item);
     DivOfShowInfo.innerHTML += `<article class="cardClass">
         <a href="${item.show.url}"><img src="${item.show.image.original}" id="imgOfInfo"></a>
         <h2>${item.show.name}</h2>
-        <p>${item.show.summary}<br>
-        rating : ${item.show.rating.average}
-        </p>
+        <p>${item.show.summary}</p>
+        
 
         </article>
         `;
   }
 }
 
-function runAllArrayOfObjectPerson(arrayOfObject) {
-  for (const item of arrayOfObject) {
-    console.log(item);
-    DivOfShowInfo.innerHTML += `<article class="cardClass">
-          <h2>${item.show.name}</h2>
-          <p>${item.show.summary}<br>
-          rating : ${item.show.rating.average}
+function runAllArrayOfObjectPerson(object) {
+  // console.log(object);
+  DivOfShowInfo.innerHTML = `<article class="cardClass">
+    <a href="${object.url}"><img src="${object.image.original}" id="imgOfInfo"></a>
+          <h2>${object.name}</h2>
           </p>
-  
           </article>
           `;
+}
+
+function runPerActorName(array) {
+  for (const iterator of array) {
+    // console.log(iterator.person);
+    DivOfShowInfo.innerHTML += `<article class="cardClass">
+   <a href="${iterator.person.url}"><img src="${iterator.person.image.original}" id="imgOfInfo"></a>
+          <h2>${iterator.person.name}</h2>
+          </p>
+   </article>
+   `;
   }
 }
 
 inputId.oninput = () => {
-  let valueOfSelect = selectId.value;
-  switch (valueOfSelect) {
-    case "shows":
-      if (inputId.value.length > 2) {
-        getMovieInfoAll()
-          .then((res) => runAllArrayOfObject(res))
-          .catch((rej) => console.log(rej));
-      }
-      break;
-      case "people":
-    default:
-      break;
+  if (inputId.value.length >= 3) {
+    switch (selectId.value) {
+      case "shows":
+          getMovieInfoAll()
+            .then((res) => runAllArrayOfObject(res))
+            .catch((rej) => console.log(rej))
+            .finally(() => {stopGifLoading()});
+      default:
+        break;
+    }
   }
+  if (inputId.value.length == 0) DivOfShowInfo.innerHTML = " ";
 };
 
 btnIDById.onclick = () => {
   sortById(inputId.value)
-    .then((res) => runAllArrayOfObject(res))
+    .then((res) => runAllArrayOfObjectPerson(res))
     .catch((rej) => console.log(rej));
+};
+
+btnIDByName.onclick = () => {
+  sortByName(inputId.value).then((res) => {
+    runPerActorName(res);
+  });
 };
